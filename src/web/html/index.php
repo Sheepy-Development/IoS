c407ac1691c7c76c8e008e83f31a58d47ce938db
CreeperCorn <mommyhelpme@mailinator.com>
2016-03-20 07:21:17 +0800

    Update index.php

--- src/web/html/index.php
+++ src/web/html/index.php
@@ -1,2 +1,134 @@
-header("Location: http://lostteam.github.io/App.html");
-die();
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Application | Maintenance</title>
+    <!-- Meta -->
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="The LEET forums are currently under maintenance. Please come back later.">
+    <link rel="shortcut icon" href="http://forum.leet.cc/favicon.ico">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./LEET _ Maintenance_files/bootstrap.min.css"> </head> <body>
+    <div id="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <!-- Info --->
+                    <h1>Sorry, we're undergoing maintenance.</h1>
+                    <h2 class="subtitle">We're currently working on the forums, please try again later.</h2><h2>
+					  <div class="form-group">
+					  </div>
+          <!-- Links -->
+          <br></h2><h1> Stay in touch. </h1>
+          <a href="http://twitter.com/leet_cc" <button="" type="submit" class="btn btn-theme">Twitter</a>
+          <a href="http://leet.cc/" <button="" type="submit" class="btn btn-theme">Website</a>
+          <a href="https://plus.google.com/+LEETSupport" <button="" type="submit" class="btn btn-theme">Google+</a>
+          <h1 class="credits">&lt;&gt; with &lt;3 by LEET, &nbsp; designed by Quantum.</h1>
+					
+				</div>
+			</div>
+				</div>
+			</div>
+		
+	
+   <style> @import url('http://fonts.googleapis.com/css?family=Lato:400,300,700,900'); body {
+	background: #66ccff;
+    font-family: 'Lato', sans-serif;
+    font-weight: 300;
+    font-size: 16px;
+    color: #555;
+    -webkit-font-smoothing: antialiased;
+    -webkit-overflow-scrolling: touch;
+    overflow-y: auto;
+}
+h1, h2, h3 {
+    font-family: 'Lato', sans-serif;
+    font-weight: 300;
+    color: #444;
+}
+h1 {
+	font-size: 40px;
+}
+a {
+    color: #ACBAC1;
+    word-wrap: break-word;
+    -webkit-transition: color 0.1s ease-in, background 0.1s ease-in;
+    -moz-transition: color 0.1s ease-in, background 0.1s ease-in;
+    -ms-transition: color 0.1s ease-in, background 0.1s ease-in;
+    -o-transition: color 0.1s ease-in, background 0.1s ease-in;
+    transition: color 0.1s ease-in, background 0.1s ease-in;
+}
+a:hover, a:focus {
+    color: #4F92AF;
+    text-decoration: none;
+    outline: 0;
+}
+a:before, a:after {
+    -webkit-transition: color 0.1s ease-in, background 0.1s ease-in;
+    -moz-transition: color 0.1s ease-in, background 0.1s ease-in;
+    -ms-transition: color 0.1s ease-in, background 0.1s ease-in;
+    -o-transition: color 0.1s ease-in, background 0.1s ease-in;
+    transition: color 0.1s ease-in, background 0.1s ease-in;
+}
+.btn {
+  display: inline-block;
+  padding: 10px 20px;
+  margin-bottom: 0;
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 1.428571429;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  cursor: pointer;
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+       -o-user-select: none;
+          user-select: none;
+  background-image: none;
+  border: 1px solid transparent;
+  border-radius: 0;
+}
+.btn-theme {
+  color: #fff;
+  background-color: #4F92AF;
+  border-color: #4F92AF;
+}
+.btn-theme:hover {
+  color: #fff;
+  background-color: #444;
+  border-color: #444;
+}
+#wrapper {
+	text-align: center;
+	background-attachment: relative;
+	background-position: center center;
+	min-height: 100px;
+	width: 100%;
+    -webkit-background-size: 100%;
+    -moz-background-size: 100%;
+    -o-background-size: 100%;
+    background-size: 100%;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+#wrapper h1 {
+	margin-top: 60px;
+	margin-bottom: 20px;
+	color: #fff;
+	font-size: 45px;
+	font-weight: 900;
+	letter-spacing: -1px;
+}
+h2.subtitle {
+	color: #fff;
+	font-size: 24px;
+}
+h1.credits {
+  padding-top: 150px;
+  font-size: 15px !important;
+}
+</style> 
+</body></html>
